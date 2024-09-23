d2b52943dfc745d54efb1c6a99a367a8d34b30fc
mrdeveloper2545 <www.Nizzerkaupist@gmail.com>
2024-09-23 14:39:00 +0300

    add this url

--- sokoni/settings.py
+++ sokoni/settings.py
@@ -168,3 +168,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/login/'
+HOME_URL = '/home/'
+LOGOUT_URL = '/logout/'
